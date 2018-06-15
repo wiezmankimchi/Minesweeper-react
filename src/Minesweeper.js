633dcc7cefa4f7b37ac4070eaf5717d4f2a7b507
wiezmankimchi <wiezmankimchi@gmail.com>
2018-06-14 17:37:13 -0700

    Initial Commit

---
+++ src/Minesweeper.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Minesweeper extends Component {
+  render() {
+    return (
+      <div className="Minesweeper">
+        <h1>Minesweeper Game</h1>
+      </div>
+    );
+  }
+}
+
+export default Minesweeper;
